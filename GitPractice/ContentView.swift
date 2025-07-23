4b47577b428a6a07ce0cb09029ac19d5db3b4374
Nandika Peddapuram <nandikarp10@gmail.com>
2025-07-23 13:44:43 -0500

    Add Change 3 to ContentView

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("My favorite color is pink!")
-            Text("My favorite color is also green!")
+            Text("My favorite color is also yellow!")
+            Text("Change 3")
         }
         .padding()
     }
